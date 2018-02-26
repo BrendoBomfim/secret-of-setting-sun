e695edec31999551e5ce5872b029a17113023472
Brendo Bomfim <brendobomfim@sciosolutions.com.br>
2018-02-25 22:57:56 -0300

    Added the firsts objects

---
+++ The Secret of Setting Sun/Assets/MovePiece.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePiece : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(0)) {
+            gameObject.transform.Translate(0, 1, 0);
+        }
+	}
+}
