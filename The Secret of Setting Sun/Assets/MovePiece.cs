e535568759de999ecd95aae4a4bcf637a86a061a
BrendoBomfim <brendobomfim@gmail.com>
2018-05-13 19:47:53 -0300

    Using raycast to move

--- The Secret of Setting Sun/Assets/MovePiece.cs
+++ The Secret of Setting Sun/Assets/MovePiece.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class MovePiece : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(0)) {
-            gameObject.transform.Translate(0, 1, 0);
-        }
-	}
+    public GameObject go;
+    void Update()
+    {
+
+
+        Vector3 v3 = Input.mousePosition;
+
+        v3.z = 10.0f;
+
+        v3 = Camera.main.ScreenToWorldPoint(v3);
+
+
+        Debug.Log(v3);
+        //Current Position of mouse in world space
+
+        gameObject.GetComponent<Rigidbody>().MovePosition(v3);
+
+
+    }
+
 }
---
+++ The Secret of Setting Sun/Assets/MovePieces.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePieces : MonoBehaviour {
+    
+        void Update()
+        {
+
+          
+            if (Input.GetButtonDown("Fire1"))
+            {
+                GetObject();
+            }
+        }
+
+        private void GetObject()
+        {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(transform.position, ray.direction, out hit, 100f)) {
+                Debug.Log(hit.transform.name);
+                Debug.DrawLine(ray.origin, hit.point);
+            if (hit.transform.tag == "Moveable") {
+                Debug.Log("Pode mexer essa desgraça");
+            }
+        }
+        }
+
+}
